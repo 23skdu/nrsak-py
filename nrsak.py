74899e5832c0757462617c11a16f8c3381cb6018
23skdu <23skdu@users.noreply.github.com>
2024-03-06 23:07:11 -0800

    add inspect module to track new subroutines

--- nrsak.py
+++ nrsak.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
-import sys, os, argparse, re, requests
+import inspect, sys, os, argparse, re, requests
 try:
   nrkey = os.environ["NR_KEY"]
 except:
   print("you must have NR_KEY environment variable set")
   sys.exit(1)
 
-valid_actions = [ 'GetSummary' , 'NRQL' ]
 nrapi = 'https://api.newrelic.com/v2'
 parser = argparse.ArgumentParser()
 parser.add_argument("action")
 args = parser.parse_args()
 action=args.action
+
+def function_list():
+  for name, data in inspect.getmembers(sys.modules['__main__'], inspect.isfunction):
+    if name.startswith('Get'):
+      print(name)
 
 def GetSummary():
   result = requests.get("https://api.newrelic.com/v2/applications.json", headers={"Api-Key":nrkey})
